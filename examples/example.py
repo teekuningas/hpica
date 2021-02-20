7fccc4ea5ce549dac002328f93cffcf10a629fe4
Erkka Heinila <erkkahe@gmail.com>
2021-02-20 15:19:02 +0200

    Improve examples

--- examples/example.py
+++ examples/example.py
@@ -6,20 +6,23 @@ from sklearn.decomposition import FastICA
 
 
 random_state = 20
-colors = ['red', 'steelblue', 'green']
+colors = ['red', 'steelblue', 'green', 'pink']
 
 ##################
 # Generate sources
-n_samples = 2000
+n_samples = 1000
 time = np.linspace(0, 8, n_samples)
 
 s1 = np.sin(2 * time)
 s2 = np.sign(np.sin(3 * time))
 s3 = scipy.signal.sawtooth(2 * np.pi * time)
+s4 = np.sin(5 * time)
 
-S = np.c_[s1, s2, s3]
-S += 0.5 * np.random.RandomState(random_state).normal(size=S.shape)
+S = np.c_[s1, s2, s3, s4]
 S = (S - S.mean(axis=0)) / S.std(axis=0)
+
+# S += 0.2 * np.random.RandomState(random_state).normal(size=S.shape)
+# S = (S - S.mean(axis=0)) / S.std(axis=0)
 
 n_sources = S.shape[1]
 
@@ -39,17 +42,29 @@ for idx in range(n_subjects):
     # padded_template_s2 = np.pad(template, pad_width=[(1,1), (0, n_subjects+1)])
     # padded_template_s3 = np.pad(template, pad_width=[(1,1), (n_subjects+1,0)])
 
-    padded_template_s1 = np.pad(template, pad_width=[(1,1),(idx+3, ((n_subjects+2-idx)))])
-    padded_template_s2 = np.pad(template, pad_width=[(1,1), (0, n_subjects+5)])
-    padded_template_s3 = np.pad(template, pad_width=[(1,1), (n_subjects+5,0)])
+    # padded_template_s1 = np.pad(template, pad_width=[(1,1), (4, n_subjects+1)])
+
+    padded_template_s1 = np.pad(template, pad_width=[(1,1),(idx+7, n_subjects+3-idx)])
+    padded_template_s2 = np.pad(template, pad_width=[(1,1), (idx+4, n_subjects+6-idx)])
+    padded_template_s3 = np.pad(template, pad_width=[(1,1), (0, n_subjects+10)])
+    padded_template_s4 = np.pad(template, pad_width=[(1,1), (n_subjects+10,0)])
 
     spatial_shape = padded_template_s1.shape
 
     A = np.array([padded_template_s1.flatten(), 
                   padded_template_s2.flatten(),
-                  padded_template_s3.flatten()]).T
+                  padded_template_s3.flatten(),
+                  padded_template_s4.flatten()]).T
 
-    X = np.dot(S, A.T)
+    subject_s1 = s1.copy()
+    subject_s2 = np.roll(s2, int(idx*n_samples/10))
+    subject_s3 = s3.copy()
+    subject_s4 = np.roll(s4, int(idx*n_samples/10))
+
+    subject_S = np.c_[subject_s1, subject_s2, subject_s3, subject_s4]
+    subject_S = (subject_S - subject_S.mean(axis=0)) / subject_S.std(axis=0)
+
+    X = np.dot(subject_S, A.T)
     X += 1.0 * np.random.RandomState(random_state).normal(size=X.shape)
     X = (X - X.mean(axis=0)) / X.std(axis=0)
 
@@ -71,22 +86,31 @@ for subject_idx, subject in enumerate(subjects):
 fig.tight_layout()
 plt.show(block=False)
 
+
 ############################
-# Helper for plotting scores
+# Helpers for plotting scores
+
+def corrcoef_with_shift(signal1, signal2):
+    corrcoefs = []
+    for idx in range(len(signal1)):
+        shifted_signal1 = np.roll(signal1, shift=idx)
+        corrcoefs.append(np.abs(np.corrcoef(shifted_signal1, signal2)[0,1]))
+    return np.max(corrcoefs)
+
 def plot_scores(sources, title):
 
     results = []
     for subject_idx in range(n_subjects):
         subject_results = []
         for unmixed_ts in sources[subject_idx].T:
-            coefs = [np.corrcoef(unmixed_ts, orig_ts)[0, 1] for orig_ts in S.T]
-            subject_results.append(np.max(np.abs(coefs)))
+            coefs = [corrcoef_with_shift(orig_ts, unmixed_ts) for orig_ts in S.T]
+            subject_results.append(np.max(coefs))
         results.append(subject_results)
 
     labels = ['Source ' + str(idx+1) for idx in range(n_sources)]
     x = np.arange(len(labels))
     width = 0.1
-    min_ = 0.8
+    min_ = 0.5
 
     fig, ax = plt.subplots()
     for subject_idx in range(n_subjects):
@@ -109,7 +133,7 @@ def plot_scores(sources, title):
 # Unmix the data with temporal concatenation and temporal ica
 concatenated = np.concatenate([data[0] for data in subjects])
 
-ica = FastICA(n_components=3)
+ica = FastICA(n_components=n_sources)
 
 sources = ica.fit_transform(concatenated)
 subjects_sources = np.vsplit(sources, n_subjects)
@@ -147,17 +171,18 @@ plot_scores(subjects_sources, title)
 # Unmix the data with spatial concatenation and temporal ICA
 concatenated = np.concatenate([data[0] for data in subjects], axis=1)
 
-ica = FastICA(n_components=3)
+ica = FastICA(n_components=n_sources)
 ica.fit(concatenated)
 
 subjects_sources = []
 subjects_mixing = []
 for subject_idx in range(n_subjects):
-    start = subject_idx*int(concatenated.shape[1]/3)
-    end = (subject_idx+1)*int(concatenated.shape[1]/3)
+    start = subject_idx*int(concatenated.shape[1]/n_subjects)
+    end = (subject_idx+1)*int(concatenated.shape[1]/n_subjects)
     mixing = ica.components_[:, start:end]
-    source = subjects[subject_idx][0] @ mixing.T
-    subjects_sources.append(source)
+    ts = subjects[subject_idx][0] @ mixing.T
+
+    subjects_sources.append(ts)
     subjects_mixing.append(mixing.T)
 
 fig, axes = plt.subplots(n_subjects, n_sources)
@@ -186,4 +211,52 @@ plt.show(block=False)
 
 title = "SC-TICA"
 plot_scores(subjects_sources, title)
+
+
+############################################
+# Unmix the data with temporal concatenation and spatial ICA
+concatenated = np.concatenate([data[0] for data in subjects], axis=0)
+
+ica = FastICA(n_components=n_sources)
+ica.fit(concatenated.T)
+
+subjects_sources = []
+subjects_mixing = []
+for subject_idx in range(n_subjects):
+    start = subject_idx*int(concatenated.shape[0]/n_subjects)
+    end = (subject_idx+1)*int(concatenated.shape[0]/n_subjects)
+    ts = ica.components_[:, start:end].T
+    mixing = ts.T @ subjects[subject_idx][0]
+
+    subjects_sources.append(ts)
+    subjects_mixing.append(mixing.T)
+
+fig, axes = plt.subplots(n_subjects, n_sources)
+fig.suptitle('TC-SICA')
+for subject_idx in range(n_subjects):
+    for source_idx in range(n_sources):
+        ax = axes[subject_idx, source_idx]
+        heatmap = subjects_mixing[subject_idx][:, source_idx].reshape(spatial_shape)
+        title = 'Source ' + str(source_idx+1)
+        ax.set_title(title)
+        ax.imshow(heatmap, cmap='hot', interpolation='nearest')
+
+fig.tight_layout()
+plt.show(block=False)
+
+fig, axes = plt.subplots(n_subjects)
+fig.suptitle('TC-SICA')
+for subject_idx, source in enumerate(subjects_sources):
+    ax = axes[subject_idx]
+    ax.set_title('Subject ' + str(subject_idx+1))
+    for ts_idx, ts in enumerate(source.T):
+        ax.plot(ts, color=colors[ts_idx], lw=0.5)
+
+fig.tight_layout()
+plt.show(block=False)
+
+title = "TC-SICA"
+plot_scores(subjects_sources, title)
+
+
 
