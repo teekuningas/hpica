d45a843d3803e56bdd187a069caecd54c88fc6f1
Erkka Heinila <erkkahe@gmail.com>
2021-04-01 14:05:35 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='hpica',
-    version='0.1.0',
+    version='0.2.0',
     description="",
     author='Erkka Heinila',
     author_email='erkka.heinila@jyu.fi',
